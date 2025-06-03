c6f828fff52f5439e0375dcb5b8c86362d35bf2a
HarryCaveMan <harris.joseph.088@gmail.com>
2025-06-03 13:30:39 -0400

    cleaning up runtime methods and adding version shim

--- cxx/include/runtime.h
+++ cxx/include/runtime.h
@@ -2,14 +2,63 @@
 
 #include <nrt/nrt.h>
 #include <cstdint>  // For uint32_t
+#include <stdexcept>  // For std::runtime_error
+#include "rust/cxx.h"  // For rust::Str, rust::Slice
 
 using std::static_cast;
 
 namespace neuronx_rs::runtime {
+    class NrtVersion {
+        public:
+            static std::unique_ptr<Version> get() {
+                auto version = std::unique_ptr<Version>(new Version());
+                return version;
+            }            
+            uint64_t major() const { return _version.rt_major; }
+            uint64_t minor() const { return _version.rt_minor; }
+            uint64_t patch() const { return _version.rt_patch; }
+            uint64_t maintenance() const { return _version.rt_maintenance; }
+            std::string detail() const { return std::string(_version.rt_detail); }
+            std::string git_hash() const { return std::string(_version.git_hash); }
+            
+        private:
+            Version() {
+                nrt_version_t *version{nullptr};
+                NRT_STATUS status = nrt_get_version(version, sizeof(nrt_version_t));
+                if (status != NRT_SUCCESS) {
+                    throw std::runtime_error("Failed to get Neuron runtime version");
+                }
+                _version = *version;
+            }
+            nrt_version_t _version;
+    };
     // Initialize neuron runtime
     // Returns NRT status code (0 for success)
     uint32_t neuronx_init() {return static_cast<uint32_t>(nrt_init(NRT_FRAMEWORK_TYPE_NO_FW,"",""));}
     // Close neuron runtime
     // Returns NRT status code (0 for success)
     uint32_t neuronx_close() {return static_cast<uint32_t>(nrt_close());}
+    // Get the version of the neuron runtime
+    unique_ptr<NrtVersion> neuronx_version() {
+        return uniqe_ptr<NrtVersion>(new NrtVersion());
+    }
+    // Get the number of neuron cores on this hardware
+    uint32_t get_nc_count() {
+        uint32_t count{0};
+        NRT_STATUS status = nrt_get_nc_count(&count);
+        if (status != NRT_SUCCESS) {
+            throw std::runtime_error("Failed to get Neuron core count");
+        }
+        return count;
+    }
+    // Get the number of visible neuron cores
+    uint32_t get_visible_nc_count() {
+        uint32_t count{0};
+        NRT_STATUS status = nrt_get_visible_nc_count(&count);
+        if (status != NRT_SUCCESS) {
+            throw std::runtime_error("Failed to get visible Neuron core count");
+        }
+        return count;
+    }
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,29 @@ pub(crate) mod ffi {
     #[namespace = "neuron_rs::model"]
     unsafe extern "C++" {
         include!("neuronx-rust/cxx/include/model.h");
+        //Model
         type Model;
-        fn load(path: &str, start_nc: i32. nc_count: i32) -> UniquePtr<Model>;
+        fn load(path: &str, start_nc: i32. nc_count: i32) -> Result<UniquePtr<Model>>;
         fn bind_slice(self: Pin<&mut Model>, usage: u32, buffer: &mut [u8]) -> u32;
         fn execute(self: self: Pin<&mut Model>);
     }
     #[namespace = "neuron_rs::runtime"]
     unsafe extern "C++" {
         include!("neuronx-rust/cxx/include/runtime.h");
-        type Runtime;
+        //Nrtversion
+        type Nrtversion;
+        fn major(self: &Version) -> u64;
+        fn minor(self: &Version) -> u64;
+        fn patch(self: &Version) -> u64;
+        fn maintenance(self: &Version) -> u64;
+        fn detail(self: &Version) -> String;
+        fn git_hash(self: &Version) -> String;
+
+        // module methods
         fn neuronx_init() -> u32;
         fn neuronx_close() -> u32;
+        fn neuronx_version() -> Result<UniquePtr<Nrtversion>>;
+        fn get_nc_count() -> Result<u32>;
+        fn get_visible_nc_count() -> Result<u32>;
     }
 }
